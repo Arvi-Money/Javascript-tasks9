22915f9610ac778d783de7927eefa320e8e8632d
sakiynam <mamatisayevasarvinoz@gmail.com>
2023-12-28 23:45:51 +0500

    Tasks

--- index.js
+++ index.js
@@ -9,17 +9,45 @@
 
 
 
-// Task - 3
-let arr = ["olma"];
 
-function palindromFunction(arg) {
-    let reverseWord = arr.reverse();
-    
-    if (arr == reverseWord) {
-        return "Palindrom so'z"
-    }else{
-        return "Palindrom so'z emas"
-    }
-}
 
-console.log(palindromFunction(arr));
+
+let str = 'hello world';
+let vowels =["a", "e", "i", "o", "u"];
+
+function vowelsFunction(arg1, arg2) {
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        let letters = str[i].split("");
+        for(let i = 0; i < letters; i++){
+            for(let i = 0; i < vowels.length; i++){
+                if (letters[i] === vowels[i]) {
+                    counter+1;
+                  }
+                }
+            }
+            }
+        
+  
+    return counter;
+  }
+  
+  console.log(vowelsFunction(str,vowels));
+
+  
+
+// // Task - 3
+// let arr = ["kiyik"];
+
+// function palindromFunction(arg) {
+//     let arr1 = arr.split("")
+//     let arr2 = arr1.reverse()
+//     let arr3 = arr2.join("")
+//     if (arr == arr3) {
+//         return "Palindrom so'z"
+//     }else{
+//         return "Palindrom so'z emas"
+//     }
+// }
+
+// console.log(palindromFunction(arr));
